25921aaa585afe6bf1c900da4fdcbac2ac8846dc
Jonathan Dart <jonathan.dart@boldcommerce.com>
2021-03-16 14:10:07 -0500

    keyboard

--- keyboards/handwired/dactyl_manuform/5x6_dart/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_dart/keymaps/default/keymap.c
@@ -20,7 +20,9 @@ void keyboard_post_init_user(void) {
 
 enum custom_keycodes {
     IDE_TEST,
-    IDE_TESTD
+    IDE_TESTD,
+    SPEC_LEFT,
+    SPEC_RIGHT
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -39,6 +41,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LSFT) "D" SS_UP(X_LCTL) SS_UP(X_LSFT)); // ctrl shift d
         }
         break;
+    case SPEC_LEFT:
+        if (record->event.pressed) {
+           SEND_STRING(SS_DOWN(X_ROPT) SS_DOWN(X_LCMD) SS_TAP(X_LEFT) SS_UP(X_ROPT) SS_UP(X_LCMD)); // ctrl shift d
+        }
+        break;
+    case SPEC_RIGHT:
+        if (record->event.pressed) {
+           SEND_STRING(SS_DOWN(X_ROPT) SS_DOWN(X_LCMD) SS_TAP(X_RIGHT) SS_UP(X_ROPT) SS_UP(X_LCMD)); // ctrl shift d
+        }
+        break;
     }
     return true;
 };
@@ -50,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB   , KC_Q  , KC_W    , KC_E  , KC_R  , KC_T ,            KC_Y  , KC_U   , KC_I  , KC_O   , KC_P  , KC_BSLASH,
      KC_ESC   , KC_A  , KC_S    , KC_D  , KC_F  , KC_G ,            KC_H  , KC_J   , KC_K  , KC_L   ,KC_SCLN, KC_QUOT,
      KC_LSHIFT, KC_Z  , KC_X    , KC_C  , KC_V  , KC_B ,            KC_N  , KC_M   ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSHIFT,
-     LOWER    , KC_GRV,IDE_TESTD,IDE_TEST,                                          KC_LBRC, KC_RBRC,KC_X   , LOWER,
+     RAISE    , KC_GRV,IDE_TESTD,IDE_TEST,                                          KC_LBRC, KC_RBRC,KC_X   , LOWER,
                                          KC_BSPC, LOWER   ,         KC_RCMD  , KC_SPC ,
                                          KC_DEL , KC_LCTL ,         KC_RCTL  , KC_ENT ,
                                          KC_BSPC, KC_LCMD ,         KC_ROPT  , KC_RCMD
@@ -60,11 +72,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
      _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,KC_VOLU,KC_VOLD,_______,_______,_______,
      _______,_______,_______,_______,_______,KC_LPRN,                        KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,_______,_______,
-     _______,_______,_______,_______,_______,_______,                        KC_HOME,KC_PGUP,KC_PGDN,KC_END ,_______,_______,
+     _______,_______,_______,_______,_______,_______,                        SPEC_LEFT,SPEC_RIGHT,KC_PGDN,KC_END ,_______,_______,
      _______,_______,_______,_______,                                                        _______,_______,_______,_______,
-                                             _______,KC_PSCR,            _______,_______,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______
+                                             _______,KC_PSCR,            KC_TRNS,_______,
+                                             _______,KC_TRNS,            KC_TRNS,_______,
+                                             _______,KC_TRNS,            KC_TRNS,KC_TRNS
 
 
 

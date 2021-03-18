14b260f9a35a3967cec37f47b2d438b5eaecc49c
Jonathan Dart <jonathan.dart@boldcommerce.com>
2021-03-18 12:21:52 -0500

    stuff

---
+++ flash.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+set -x
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+qmk compile -km default -kb handwired/dactyl_manuform/5x6_dart
+
+
+echo "reset it"
+
+sleep 5
+
+
+
+
+while true; do
+    dev=$(find /dev -maxdepth 1 -name 'cu.usbmodem*' | grep -v 142101)
+    if [ -n "$dev" ]; then
+        /Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude \
+            -C/Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf -v \
+            -patmega32u4 -cavr109 -P$dev -b57600 -D \
+            -Uflash:w:/Users/jonathan.dart/repo/qmk_firmware/handwired_dactyl_manuform_5x6_dart_default.hex:i
+        exit
+    fi
+done
+
+# while true; do
+#     # Find the Arduino port
+#     ARDUINO_UPLOAD_PORT="$(find /dev/cu.usbmodem* | grep -v 142101 | head -n 1)"
+#     if [ -n "$dev" ]; then
+#         stty -f "${ARDUINO_UPLOAD_PORT}" 1200
+#         sleep 0.5
+
+
+#     fi
+# done
+
+
+# Reset the Arduino
--- keyboards/handwired/dactyl_manuform/5x6_dart/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_dart/keymaps/default/keymap.c
@@ -19,10 +19,12 @@ void keyboard_post_init_user(void) {
 }
 
 enum custom_keycodes {
-    IDE_TEST,
+    IDE_TEST = SAFE_RANGE,
     IDE_TESTD,
     SPEC_LEFT,
-    SPEC_RIGHT
+    SPEC_RIGHT,
+    PSS_NEXT,
+    PSS_PREV
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -31,26 +33,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
 #endif
     switch (keycode) {
-    case IDE_TEST:
-        if (record->event.pressed) {
-           SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LSFT) "R" SS_UP(X_LCTL) SS_UP(X_LSFT)); // ctrl shift r
-        }
-        break;
-    case IDE_TESTD:
-        if (record->event.pressed) {
-           SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LSFT) "D" SS_UP(X_LCTL) SS_UP(X_LSFT)); // ctrl shift d
-        }
-        break;
-    case SPEC_LEFT:
-        if (record->event.pressed) {
-           SEND_STRING(SS_DOWN(X_ROPT) SS_DOWN(X_LCMD) SS_TAP(X_LEFT) SS_UP(X_ROPT) SS_UP(X_LCMD)); // ctrl shift d
-        }
-        break;
-    case SPEC_RIGHT:
-        if (record->event.pressed) {
-           SEND_STRING(SS_DOWN(X_ROPT) SS_DOWN(X_LCMD) SS_TAP(X_RIGHT) SS_UP(X_ROPT) SS_UP(X_LCMD)); // ctrl shift d
-        }
-        break;
+        case IDE_TEST:
+            if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LSFT) "R" SS_UP(X_LCTL) SS_UP(X_LSFT)); // ctrl shift r
+            }
+            break;
+        case IDE_TESTD:
+            if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LSFT) "D" SS_UP(X_LCTL) SS_UP(X_LSFT)); // ctrl shift d
+            }
+            break;
+        case SPEC_LEFT:
+            if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_ROPT) SS_DOWN(X_LCMD) SS_TAP(X_LEFT) SS_UP(X_ROPT) SS_UP(X_LCMD)); // ctrl shift d
+            }
+            break;
+        case SPEC_RIGHT:
+            if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_ROPT) SS_DOWN(X_LCMD) SS_TAP(X_RIGHT) SS_UP(X_ROPT) SS_UP(X_LCMD)); // ctrl shift d
+            }
+            break;
+        case PSS_NEXT:
+            if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_ROPT) SS_DOWN(X_LCMD) SS_TAP(X_DOWN) SS_UP(X_ROPT) SS_UP(X_LCMD)); // ctrl shift d
+            }
+            break;
+        case PSS_PREV:
+            if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_ROPT) SS_DOWN(X_LCMD) SS_TAP(X_UP) SS_UP(X_ROPT) SS_UP(X_LCMD)); // ctrl shift d
+            }
+            break;
     }
     return true;
 };
@@ -62,10 +74,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB   , KC_Q  , KC_W    , KC_E  , KC_R  , KC_T ,            KC_Y  , KC_U   , KC_I  , KC_O   , KC_P  , KC_BSLASH,
      KC_ESC   , KC_A  , KC_S    , KC_D  , KC_F  , KC_G ,            KC_H  , KC_J   , KC_K  , KC_L   ,KC_SCLN, KC_QUOT,
      KC_LSHIFT, KC_Z  , KC_X    , KC_C  , KC_V  , KC_B ,            KC_N  , KC_M   ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSHIFT,
-     RAISE    , KC_GRV,IDE_TESTD,IDE_TEST,                                          KC_LBRC, KC_RBRC,KC_X   , LOWER,
+     IDE_TESTD,IDE_TEST,RAISE   , KC_GRV,                                           KC_LBRC, KC_RBRC,PSS_PREV,PSS_NEXT,
                                          KC_BSPC, LOWER   ,         KC_RCMD  , KC_SPC ,
-                                         KC_DEL , KC_LCTL ,         KC_RCTL  , KC_ENT ,
-                                         KC_BSPC, KC_LCMD ,         KC_ROPT  , KC_RCMD
+                                         KC_DEL , RAISE   ,         KC_RCTL  , KC_ENT ,
+                                         KC_LCTL, KC_LCMD ,         KC_ROPT  , KC_RCMD
   ),
 
   [_LOWER] = LAYOUT_5x6_dart(
@@ -73,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,KC_VOLU,KC_VOLD,_______,_______,_______,
      _______,_______,_______,_______,_______,KC_LPRN,                        KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,_______,_______,
      _______,_______,_______,_______,_______,_______,                        SPEC_LEFT,SPEC_RIGHT,KC_PGDN,KC_END ,_______,_______,
-     _______,_______,_______,_______,                                                        _______,_______,_______,_______,
+     _______,_______,_______,_______,                                            _______,_______,_______,_______,
                                              _______,KC_PSCR,            KC_TRNS,_______,
                                              _______,KC_TRNS,            KC_TRNS,_______,
                                              _______,KC_TRNS,            KC_TRNS,KC_TRNS
@@ -84,8 +96,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_5x6_dart(
        KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-       _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
+       _______,_______,_______,_______,_______,KC_LBRC,                        _______,KC_MS_BTN1,KC_MS_BTN2 ,KC_SLCK,KC_MUTE,_______,
+       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_MS_LEFT,KC_MS_DOWN,KC_MS_UP,KC_MS_RIGHT,KC_MNXT,KC_VOLU,
        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
        _______,_______,_______,_______,                                                        _______,_______,_______,_______,
                                                _______,_______,            KC_EQL ,_______,
